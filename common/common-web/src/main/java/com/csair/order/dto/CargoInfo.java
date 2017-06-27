15762a1ab198d1d3cc8f85064afba42c14cbca19
jxw1989 <jinxiuwei.1988@163.com>
2017-06-27 15:14:20 +0800

    代码优化

---
+++ common/common-web/src/main/java/com/csair/order/dto/CargoInfo.java
@@ -0,0 +1,51 @@
+/*
+ * 文 件 名:  CargoInfo.java
+ * 版    权:  China Southern Airlines Information Center. Copyright ,  All rights reserved
+ * 描    述:  <描述>
+ * 修 改 人:  jinxiuwei
+ * 修改时间:  Jun 27, 2017
+ * 跟踪单号:  <跟踪单号>
+ * 修改单号:  <修改单号>
+ * 修改内容:  <修改内容>
+ */
+package com.csair.order.dto;
+
+/**
+ * <一句话功能简述> <功能详细描述>
+ * 
+ * @author jinxiuwei
+ * @version [版本号, Jun 27, 2017]
+ * @see [相关类/方法]
+ * @since [产品/模块版本]
+ */
+public class CargoInfo
+{
+    private String goodsType;
+    
+    private String piece;
+    
+    private String weight;
+    
+    public CargoInfo(String goodsType, String piece, String weight)
+    {
+        this.goodsType = goodsType;
+        this.piece = piece;
+        this.weight = weight;
+    }
+    
+    public String getGoodsType()
+    {
+        return goodsType;
+    }
+    
+    public String getPiece()
+    {
+        return piece;
+    }
+    
+    public String getWeight()
+    {
+        return weight;
+    }
+    
+}
