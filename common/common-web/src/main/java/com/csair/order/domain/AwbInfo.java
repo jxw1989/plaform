1a66dbfe23789de5a8363b15de6f176b579eab13
jxw1989 <jinxiuwei.1988@163.com>
2017-07-03 15:43:23 +0800

    代码提交

---
+++ common/common-web/src/main/java/com/csair/order/domain/AwbInfo.java
@@ -0,0 +1,71 @@
+/*
+ * 文 件 名:  AwbInfo.java
+ * 版    权:  Copyright @ China Southern Airlines Information Center Ltd ,  All rights reserved
+ * 描    述:  <描述>
+ * 修 改 人:  jinxiuwei
+ * 修改时间:  Jul 3, 2017
+ * 跟踪单号:  <跟踪单号>
+ * 修改单号:  <修改单号>
+ * 修改内容:  <修改内容>
+ */
+package com.csair.order.domain;
+
+/**
+ * <一句话功能简述> <功能详细描述>
+ * 
+ * @author jinxiuwei
+ * @version [版本号, Jul 3, 2017]
+ * @see [相关类/方法]
+ * @since [产品/模块版本]
+ */
+public class AwbInfo
+{
+    private String orderId;
+    
+    private String awboPrefix;
+    
+    private String awbNo;
+    
+    private String awbPostFix;
+    
+    public String getOrderId()
+    {
+        return orderId;
+    }
+    
+    public void setOrderId(String orderId)
+    {
+        this.orderId = orderId;
+    }
+    
+    public String getAwboPrefix()
+    {
+        return awboPrefix;
+    }
+    
+    public void setAwboPrefix(String awboPrefix)
+    {
+        this.awboPrefix = awboPrefix;
+    }
+    
+    public String getAwbNo()
+    {
+        return awbNo;
+    }
+    
+    public void setAwbNo(String awbNo)
+    {
+        this.awbNo = awbNo;
+    }
+    
+    public String getAwbPostFix()
+    {
+        return awbPostFix;
+    }
+    
+    public void setAwbPostFix(String awbPostFix)
+    {
+        this.awbPostFix = awbPostFix;
+    }
+    
+}
---
+++ common/common-web/src/main/java/com/csair/order/domain/FlightInfo.java
@@ -0,0 +1,109 @@
+/*
+ * 文 件 名:  FlightInfo.java
+ * 版    权:  Copyright @ China Southern Airlines Information Center Ltd ,  All rights reserved
+ * 描    述:  <描述>
+ * 修 改 人:  jinxiuwei
+ * 修改时间:  Jul 3, 2017
+ * 跟踪单号:  <跟踪单号>
+ * 修改单号:  <修改单号>
+ * 修改内容:  <修改内容>
+ */
+package com.csair.order.domain;
+
+import java.util.Date;
+
+/**
+ * <一句话功能简述> <功能详细描述>
+ * 
+ * @author jinxiuwei
+ * @version [版本号, Jul 3, 2017]
+ * @see [相关类/方法]
+ * @since [产品/模块版本]
+ */
+public class FlightInfo
+{
+    private String orderId;
+    
+    private String flightNo;
+    
+    private Date flightDate;
+    
+    private String dep;
+    
+    private String des;
+    
+    private Date depTime;
+    
+    private Date desTime;
+    
+    public String getOrderId()
+    {
+        return orderId;
+    }
+    
+    public void setOrderId(String orderId)
+    {
+        this.orderId = orderId;
+    }
+    
+    public String getFlightNo()
+    {
+        return flightNo;
+    }
+    
+    public void setFlightNo(String flightNo)
+    {
+        this.flightNo = flightNo;
+    }
+    
+    public Date getFlightDate()
+    {
+        return flightDate;
+    }
+    
+    public void setFlightDate(Date flightDate)
+    {
+        this.flightDate = flightDate;
+    }
+    
+    public String getDep()
+    {
+        return dep;
+    }
+    
+    public void setDep(String dep)
+    {
+        this.dep = dep;
+    }
+    
+    public String getDes()
+    {
+        return des;
+    }
+    
+    public void setDes(String des)
+    {
+        this.des = des;
+    }
+    
+    public Date getDepTime()
+    {
+        return depTime;
+    }
+    
+    public void setDepTime(Date depTime)
+    {
+        this.depTime = depTime;
+    }
+    
+    public Date getDesTime()
+    {
+        return desTime;
+    }
+    
+    public void setDesTime(Date desTime)
+    {
+        this.desTime = desTime;
+    }
+    
+}
